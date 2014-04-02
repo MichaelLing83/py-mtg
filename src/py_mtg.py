0b5d89fd837fae6a1957ffa7f2b18b7d9a15ae81
duo.ling.cn@gmail.com <duo.ling.cn@gmail.com>
2014-04-02 19:27:32 +0200

    add parsing.

--- src/py_mtg.py
+++ src/py_mtg.py
@@ -1,5 +1,6 @@
 ﻿import json
 import encodings.utf_8
+import sys
 
 class MtgDataBase:
     '''
@@ -9,18 +10,28 @@ class MtgDataBase:
         '''
         '''
         dbFile = open(json_file_name, 'r', encoding="utf-8")
-        self.mtgDb = json.load(dbFile)
+        self.__rawMtgDb = json.load(dbFile)
         dbFile.close()
+        self.__mtgDb = dict()
+        for set_name in self.__rawMtgDb.keys():
+            for card in self.__rawMtgDb.get(set_name).get("cards"):
+                if card.get("name") not in self.__mtgDb.keys():
+                    self.__mtgDb[card.get("name")] = card
+    
+    def get_all_card_names(self):
+        '''
+        Return a list of all card names.
+        '''
+        return self.__mtgDb.keys()
+    
+    def get_db(self):
+        return self.__mtgDb
     
     def get_card_by_name(self, name):
         '''
         Get an card by it's precise name.
         '''
-        for set_name in self.mtgDb.keys():
-            for card in self.mtgDb.get(set_name).get("cards"):
-                if card.get("name") == name:
-                    return card
-        raise ValueError('Card name "%s" is not found!' % name)
+        return self.__mtgDb[name]
     
     def print_card(self, card):
         '''
@@ -30,6 +41,8 @@ class MtgDataBase:
         for key in card.keys():
             if key != "foreignNames":
                 s += "%s:\t%s\n" % (key, card.get(key))
+            #else:
+                #sys.displayhook(card.get(key))
         return s
 
 if __name__ == '__main__':
