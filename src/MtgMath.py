fac31b1fa15d37790c78f29bc8606de7141f30a2
Michael Duo Ling <duo.ling.cn@gmail.com>
2014-04-13 20:39:37 +0200

    Add MtgMath for math functions.

---
+++ src/MtgMath.py
@@ -0,0 +1,38 @@
+﻿
+class MtgMath:
+    '''
+    Mathematics methods. 
+    '''
+
+    __n_factorial__ = dict()
+    __n_factorial_max_key__ = 0
+    __n_factorial__[__n_factorial_max_key__] = 1
+    
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def C(cls, n, r):
+        '''
+        Number of possible solutions of "n choose r", i.e. Choose r items from total n items.
+        '''
+        if r > n or n < 0 or r < 0:
+            raise ValueError("Invalid input for 'n choose r' calculation with n=%d, r=%d." % (n, r))
+        return MtgMath.n_factorial(n) / ( MtgMath.n_factorial(r) * MtgMath.n_factorial(n-r) )
+    
+    @classmethod
+    def n_factorial(cls, n):
+        '''
+        Return "n factorial".
+        
+        @return n! (int): note 0!=1
+        '''
+        if n < 0:
+            raise ValueError("n=%d is not valid for factorial calculation!" % n)
+        elif n == 0:
+            return 1
+        else:
+            if n > MtgMath.__n_factorial_max_key__:
+                for x in range(MtgMath.__n_factorial_max_key__+1, n+1):
+                    MtgMath.__n_factorial__[x] = MtgMath.__n_factorial__[x-1] * x
+            return MtgMath.__n_factorial__[n]
---
+++ src/testMtgMath.py
@@ -0,0 +1,36 @@
+﻿import unittest
+from MtgMath import MtgMath
+
+class TestMtgMath(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("\nStart testing %s" % __name__)
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("\nFinish testing %s" % __name__)
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_n_factorial(self):
+        '''
+        Verify n! calculation is correct.
+        '''
+        self.assertEqual(MtgMath.n_factorial(0), 1)
+        self.assertEqual(MtgMath.n_factorial(10), 3628800)
+    
+    def test_C(self):
+        '''
+        Verify C(n, r) calculation.
+        '''
+        self.assertEqual(MtgMath.C(1, 1), 1)
+        self.assertEqual(MtgMath.C(0, 0), 1)
+        self.assertEqual(MtgMath.C(10, 2), 45)
+
+if __name__ == '__main__':
+    unittest.main()
