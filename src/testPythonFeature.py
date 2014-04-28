6dd5708068fe575249975cd32c030aea52ea6391
duo.ling.cn@gmail.com <duo.ling.cn@gmail.com>
2014-04-28 16:06:55 +0200

    Add testing Python feature, so we understand which feature of the language is a must for us.

---
+++ src/testPythonFeature.py
@@ -0,0 +1,39 @@
+﻿import unittest
+from typecheck import *
+import types
+
+class TestPythonFeature(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("\nStart testing %s" % __name__)
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("\nFinish testing %s" % __name__)
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_method_redefine(self):
+        '''
+        Verify that object methods can be redefined on-the-fly.
+        '''
+        class A:
+            def __init__(self):
+                pass
+            def say_hi(self):
+                return "hi"
+        
+        a = A()
+        self.assertEqual(a.say_hi(), "hi")
+        def say_hi(self):
+            return "hej"
+        a.__setattr__(say_hi.__name__, types.MethodType(say_hi, a))
+        self.assertEqual(a.say_hi(), "hej")
+
+if __name__ == '__main__':
+    unittest.main()
