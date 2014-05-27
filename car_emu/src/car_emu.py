22fc3de437dfac3e732f97dc7c8d7cac0eb0d548
duo.ling.cn@gmail.com <duo.ling.cn@gmail.com>
2014-05-27 10:11:40 +0200

    Add car_emu.py and an empty framework to update canvas on time intervals.

---
+++ car_emu/src/car_emu.py
@@ -0,0 +1,19 @@
+﻿'''
+'''
+
+if __name__ == '__main__':
+    from tkinter import Tk
+    from tkinter import Canvas
+    from tkinter import mainloop
+    
+    master = Tk()
+    canvas = Canvas(master, width=200, height=200)
+    canvas.pack()
+    r = canvas.create_rectangle(0, 0, 20, 20, fill="blue")
+    
+    def ppp():
+        canvas.move(r, 5, 5)
+        canvas.after(1000, ppp)
+    canvas.after(1000, ppp)
+    
+    master.mainloop()
